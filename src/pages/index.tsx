f49c0a297a0ee038efc4eede7d33d6936f8e6c87
Jón Gabríel Lorange <jonl17@ru.is>
2022-09-11 14:26:07 +0000

    bare bones + tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  return (
+    <div className='p-4'>
+      <h1>ungfrú reykjavík</h1>
+    </div>
+  );
+};
+
+export default Home;
