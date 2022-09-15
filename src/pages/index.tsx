30c69f473aa77b8da782a7e14cb95802dc059da6
Jón Gabríel Lorange <jonl17@ru.is>
2022-09-15 21:21:29 +0000

    marquee speeds

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Marquee from 'react-fast-marquee'
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className="overflow-hidden ">
       <SEO />
       <div className="min-h-screen max-w-lg mx-auto grid place-content-center">
         <div className="absolute left-10 top-10">
@@ -29,16 +29,29 @@ const Home: NextPage = () => {
           </h1>
         </motion.div>
       </div>
-      <Marquee
-        speed={65}
-        gradient={false}
-        className="rotate-[11.5deg] absolute bottom-56 md:bottom-44"
-      >
-        <div className="font-edit-italic mt-2 md:mt-0 pl-0 text-[22px] md:text-[53px] tracking-[8px] md:tracking-[16px]">
-          <p>skál fyrir svísum</p>
-        </div>
-      </Marquee>
-    </>
+      <div className="hidden md:block ">
+        <Marquee
+          speed={65}
+          gradient={false}
+          className="rotate-[11.5deg] absolute bottom-56 -left-5 md:bottom-44"
+        >
+          <div className="font-edit-italic mt-2 md:mt-0 text-[22px] md:text-[53px] tracking-[8px] md:tracking-[16px]">
+            <p>skál fyrir svísum</p>
+          </div>
+        </Marquee>
+      </div>
+      <div className="block md:hidden">
+        <Marquee
+          speed={35}
+          gradient={false}
+          className="rotate-[11.5deg] absolute bottom-56 -left-5 md:bottom-44"
+        >
+          <div className="font-edit-italic mt-2 md:mt-0 text-[22px] md:text-[53px] tracking-[8px] md:tracking-[16px]">
+            <p>skál fyrir svísum</p>
+          </div>
+        </Marquee>
+      </div>
+    </div>
   )
 }
 
