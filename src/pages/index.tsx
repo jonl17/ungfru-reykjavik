f85c6d686f7b35665b7f500be53bccb528ae1e16
Jón Gabríel Lorange <jonl17@ru.is>
2022-09-17 13:00:09 +0000

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,36 @@
 import type { NextPage } from 'next'
 import { SEO, Logo } from '~/components'
-import { motion } from 'framer-motion'
-import Marquee from 'react-fast-marquee'
 import MockupSvg from '~/components/MockupSvg'
+
+const facebookUrl = 'https://www.facebook.com/profile.php?id=100083072875016'
+const instagramUrl = 'https://www.instagram.com/ungfrureykjavik/'
 
 const Home: NextPage = () => {
   return (
     <div className="overflow-hidden ">
       <SEO />
-      <div className="min-h-screen px-4 md:max-w-xl 3xl:max-w-3xl mx-auto grid place-content-center">
+      <div className="min-h-screen px-4 md:max-w-xl 2xl:max-w-3xl mx-auto grid place-content-center">
         <div className="absolute left-5 top-5 md:left-10 md:top-10 h-20 w-20 md:h-28 md:w-28">
           <Logo />
         </div>
-        <div className="w-full">
+        <div className="w-full relative">
           <MockupSvg />
+          <div className="h-full w-full absolute top-0 left-0 flex place-items-end">
+            <a
+              className="h-20 w-full block"
+              href={facebookUrl}
+              target="_blank"
+              rel="noreferrer"
+              aria-label="facebook link"
+            />
+            <a
+              className="h-20 w-full block"
+              href={instagramUrl}
+              target="_blank"
+              rel="noreferrer"
+              aria-label="instagram-link"
+            />
+          </div>
         </div>
       </div>
     </div>
