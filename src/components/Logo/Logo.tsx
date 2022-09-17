9b0c586b7ce7d4d0138a5e9d5b0314282d243de8
Jón Gabríel Lorange <jonl17@ru.is>
2022-09-17 12:47:45 +0000

    svg image

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,8 +2,8 @@ export default function Logo() {
   return (
     <div>
       <svg
-        width="108"
-        height="85"
+        width="100%"
+        height="100%"
         viewBox="0 0 108 85"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,54 +2,19 @@ import type { NextPage } from 'next'
 import { SEO, Logo } from '~/components'
 import { motion } from 'framer-motion'
 import Marquee from 'react-fast-marquee'
+import MockupSvg from '~/components/MockupSvg'
 
 const Home: NextPage = () => {
   return (
     <div className="overflow-hidden ">
       <SEO />
-      <div className="min-h-screen max-w-lg mx-auto grid place-content-center">
-        <div className="absolute left-10 top-10">
+      <div className="min-h-screen px-4 md:max-w-xl 3xl:max-w-3xl mx-auto grid place-content-center">
+        <div className="absolute left-5 top-5 md:left-10 md:top-10 h-20 w-20 md:h-28 md:w-28">
           <Logo />
         </div>
-        <div className="flex justify-between uppercase mx-auto md:mx-0 px-4 w-[355px] md:max-w-none md:w-auto text-[35px] md:text-[89px] leading-[0.8] tracking-[8px]">
-          <h1>Hér</h1>
-          <h1>Opnar</h1>
+        <div className="w-full">
+          <MockupSvg />
         </div>
-
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.4 }}
-        >
-          <h1 className="text-60px/1.5 md:text-120px/1.6 uppercase text-center text-transparent gold-bg page-title-top-tweak-mobile md:page-title-top-tweak">
-            Ungfrú
-          </h1>
-          <h1 className="text-60px/1.5 md:text-120px/1.6 uppercase text-center text-transparent gold-bg">
-            Reykjavík
-          </h1>
-        </motion.div>
-      </div>
-      <div className="hidden md:block ">
-        <Marquee
-          speed={65}
-          gradient={false}
-          className="rotate-[11.5deg] absolute bottom-56 -left-5 md:bottom-44"
-        >
-          <div className="font-edit-italic mt-2 md:mt-0 text-[22px] md:text-[53px] tracking-[8px] md:tracking-[16px]">
-            <p>skál fyrir svísum</p>
-          </div>
-        </Marquee>
-      </div>
-      <div className="block md:hidden">
-        <Marquee
-          speed={35}
-          gradient={false}
-          className="rotate-[11.5deg] absolute bottom-56 -left-5 md:bottom-44"
-        >
-          <div className="font-edit-italic mt-2 md:mt-0 text-[22px] md:text-[53px] tracking-[8px] md:tracking-[16px]">
-            <p>skál fyrir svísum</p>
-          </div>
-        </Marquee>
       </div>
     </div>
   )
