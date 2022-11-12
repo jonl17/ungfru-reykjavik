ba36c6c8cb36ad43f508b4807a5f34b188b7e577
lorange <jglorange@gmail.com>
2022-11-12 11:35:58 +0000

    duratijion

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export default function Button({ children }: Props) {
   return (
-    <span className="transition-all text-28px/1 uppercase bg-black text-orange px-5 py-3 w-full grid place-items-center shadow-[0_0_0_2px_black] hover:text-black hover:bg-orange">
+    <span className="transition-all duration-500 text-28px/1 uppercase bg-black text-orange px-5 py-3 w-full grid place-items-center shadow-[0_0_0_2px_black] hover:text-black hover:bg-orange">
       <p className="mt-1">{children}</p>
     </span>
   )
